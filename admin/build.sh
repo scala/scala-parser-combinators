34bbdaa03e7e57a6572caed45eda30ba6e913812
Lukas Rytz <lukas.rytz@gmail.com>
2015-05-11 16:47:27 +0200

    Tag-driven publishing, v2
    Scripts taken from here:
    https://github.com/scala/scala-java8-compat/commit/4a6cfc97cd95227b86650410e1b632e5ff79335b
    
    New keys generated as described in the README.

---
+++ admin/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+# prep environment for publish to sonatype staging if the HEAD commit is tagged
+
+# git on travis does not fetch tags, but we have TRAVIS_TAG
+# headTag=$(git describe --exact-match ||:)
+
+if [ "$TRAVIS_JDK_VERSION" == "$PUBLISH_JDK" ] && [[ "$TRAVIS_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)? ]]; then
+  echo "Going to release from tag $TRAVIS_TAG!"
+  myVer=$(echo $TRAVIS_TAG | sed -e s/^v//)
+  publishVersion='set every version := "'$myVer'"'
+  extraTarget="publish-signed"
+  cat admin/gpg.sbt >> project/plugins.sbt
+  cp admin/publish-settings.sbt .
+
+  # Copied from the output of genKeyPair.sh
+  K=$encrypted_5e972ec514e2_key
+  IV=$encrypted_5e972ec514e2_iv
+
+  openssl aes-256-cbc -K $K -iv $IV -in admin/secring.asc.enc -out admin/secring.asc -d
+fi
+
+sbt "$publishVersion" clean update test publishLocal $extraTarget
---
+++ admin/gpg.sbt
@@ -0,0 +1,2 @@
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3") // only added when publishing, see build.sh
