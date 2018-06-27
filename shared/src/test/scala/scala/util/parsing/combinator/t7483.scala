81aa38c5cbb3278d734e9026c2f2ae512134a974
Philippus <philippus@gmail.com>
2018-06-27 22:44:12 +0200

    Add test showing scala/bug#7483 is fixed

---
+++ shared/src/test/scala/scala/util/parsing/combinator/t7483.scala
@@ -0,0 +1,17 @@
+package scala.util.parsing.combinator
+
+import scala.util.parsing.input.{CharSequenceReader, OffsetPosition}
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class t7483 {
+  val s = "foo\nbar"
+  val reader = new CharSequenceReader(s.toCharArray(), 0)
+  val p = reader.pos.asInstanceOf[OffsetPosition]
+
+  @Test
+  def test: Unit = {
+    assertEquals("foo", p.lineContents)
+  }
+}
