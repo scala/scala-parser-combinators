6c7804bb3d255334f4be0e856d5d9257e3860f70
Philippus <philippus@gmail.com>
2018-06-27 22:30:47 +0200

    Implement ~> and <~ for OnceParser, fixes scala/bug#6464

---
+++ shared/src/test/scala/scala/util/parsing/combinator/t6464.scala
@@ -0,0 +1,39 @@
+import scala.util.parsing.input.CharSequenceReader
+import scala.util.parsing.combinator.RegexParsers
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class t6464 {
+  object SspParser extends RegexParsers {
+    val ok: Parser[Any] =
+      ("<%" ~! rep(' ') ~ "\\w+".r ~ rep(' ') ~ "%>"
+        | "<%" ~! err("should not fail here, because of ~!"))
+
+    val buggy: Parser[Any] =
+      ("<%" ~! rep(' ') ~> "\\w+".r <~ rep(' ') ~ "%>"
+        | "<%" ~! err("should not fail here, because of ~!"))
+
+  }
+    @Test
+    def test: Unit = {
+     assertEquals(
+        "[1.9] parsed: ((((<%~List( ))~hi)~List( ))~%>)",
+        SspParser.phrase(SspParser.ok)(new CharSequenceReader("<% hi %>")).toString)
+      val expected = """[1.7] error: string matching regex '\w+' expected but '%' found
+
+<%    %>
+      ^"""
+      assertEquals(
+        expected,
+        SspParser.phrase(SspParser.ok)(new CharSequenceReader("<%    %>")).toString)
+
+      assertEquals(
+        "[1.9] parsed: hi",
+        SspParser.phrase(SspParser.buggy)(new CharSequenceReader("<% hi %>")).toString)
+
+      assertEquals(
+        expected,
+        SspParser.phrase(SspParser.buggy)(new CharSequenceReader("<%    %>")).toString)
+  }
+}
