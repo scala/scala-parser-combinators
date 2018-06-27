e55975594bf8ec418f35fc3cef312b6e8d07267d
Philippus <philippus@gmail.com>
2018-06-27 23:10:26 +0200

    Add test showing scala/bug#3212 is fixed

---
+++ shared/src/test/scala/scala/util/parsing/combinator/t3212.scala
@@ -0,0 +1,27 @@
+package scala.util.parsing.combinator
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class t3212 extends RegexParsers {
+
+    sealed trait BuySell
+    case object BUY extends BuySell
+    case object SELL extends BuySell
+
+    def buy_sell: Parser[BuySell] =
+      "to" ~> "buy" ^^^ BUY |
+      "to" ~> "sell" ^^^ SELL |
+      failure("buy or sell expected")
+
+  @Test
+  def test: Unit = {
+    val parseResult = parse[BuySell](phrase(buy_sell), "bought")
+
+    val expected = """[1.1] failure: buy or sell expected
+
+bought
+^"""
+    assertEquals(expected, parseResult.toString)
+  }
+}
