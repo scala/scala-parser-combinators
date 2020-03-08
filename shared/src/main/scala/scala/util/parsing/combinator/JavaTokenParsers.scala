28a2798dbbad3ebad15cb197ae1e9a66b1e98532
Philippus <philippus@gmail.com>
2020-03-08 12:00:50 +0100

    Add import clauses for ImplicitConversions

--- shared/src/main/scala/scala/util/parsing/combinator/JavaTokenParsers.scala
+++ shared/src/main/scala/scala/util/parsing/combinator/JavaTokenParsers.scala
@@ -14,6 +14,7 @@ package scala
 package util.parsing.combinator
 
 import scala.annotation.migration
+import scala.language.implicitConversions
 
 /** `JavaTokenParsers` differs from [[scala.util.parsing.combinator.RegexParsers]]
  *  by adding the following definitions:
