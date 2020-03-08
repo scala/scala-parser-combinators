96e8a8872c7bf2d1c5b9ae2169155414fd4de522
Philippus <philippus@gmail.com>
2020-03-08 14:07:22 +0100

    Add cases for exhaustivity

--- shared/src/test/scala/scala/util/parsing/combinator/PackratParsersTest.scala
+++ shared/src/test/scala/scala/util/parsing/combinator/PackratParsersTest.scala
@@ -28,6 +28,8 @@ class PackratParsersTest {
     def extractResult(r : ParseResult[Int]): Int = r match {
       case Success(a,_) => a
       case NoSuccess(a,_) => sys.error(a)
+      case Failure(a, _) => sys.error(a)
+      case Error(a, _) => sys.error(a)
     }
     def check(expected: Int, expr: String): Unit = {
       val parseResult = head(new lexical.Scanner(expr))
@@ -82,6 +84,8 @@ class PackratParsersTest {
     def extractResult(r : ParseResult[Int]): Int = r match {
       case Success(a,_) => a
       case NoSuccess(a,_) => sys.error(a)
+      case Failure(a, _) => sys.error(a)
+      case Error(a, _) => sys.error(a)
     }
     def check(expected: Int, expr: String): Unit = {
       val parseResult = head(new lexical.Scanner(expr))
@@ -105,6 +109,8 @@ class PackratParsersTest {
     def extractResult(r: ParseResult[AnBnCnResult]): AnBnCnResult = r match {
       case Success(a,_) => a
       case NoSuccess(a,_) => sys.error(a)
+      case Failure(a, _) => sys.error(a)
+      case Error(a, _) => sys.error(a)
     }
     def threeLists(as: List[Symbol], bs: List[Symbol], cs: List[Symbol]): AnBnCnResult = {
       val as1 = as.map(_.name)
