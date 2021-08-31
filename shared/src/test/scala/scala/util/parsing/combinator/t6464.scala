70d57395844392deaabc7bb260ee7ee9c7450ff0
Seth Tisue <seth@tisue.net>
2021-08-31 17:12:09 -0400

    add headers to test sources

--- shared/src/test/scala/scala/util/parsing/combinator/t6464.scala
+++ shared/src/test/scala/scala/util/parsing/combinator/t6464.scala
@@ -1,3 +1,15 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
 import scala.language.implicitConversions
 import scala.util.parsing.input.CharSequenceReader
 import scala.util.parsing.combinator.RegexParsers
